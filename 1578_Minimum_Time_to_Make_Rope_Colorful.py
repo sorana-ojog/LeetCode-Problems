f442ad62ac6ffd14b9e4a2fe32e419b3be328c45
Sorana Ojog <81490906+sorana-ojog@users.noreply.github.com>
2022-10-03 20:53:56 +0300

    Create 1578_Minimum_Time_to_Make_Rope_Colorful.py

---
+++ 1578_Minimum_Time_to_Make_Rope_Colorful.py
@@ -0,0 +1,38 @@
+'''
+1578. Minimum Time to Make Rope Colorful
+
+Alice has n balloons arranged on a rope. You are given a 0-indexed string colors 
+where colors[i] is the color of the ith balloon.
+
+Alice wants the rope to be colorful. She does not want two consecutive balloons to be of the same color, 
+so she asks Bob for help. Bob can remove some balloons from the rope to make it colorful. You are given 
+a 0-indexed integer array neededTime where neededTime[i] is the time (in seconds) that Bob needs to 
+remove the ith balloon from the rope.
+
+Return the minimum time Bob needs to make the rope colorful.
+'''
+
+from math import inf
+class Solution:
+    def minCost(self, colors: str, neededTime: List[int]) -> int:
+        totalNeededTime = 0
+        maxTime = 0
+        currentSum = 0
+        colors += "!"
+        
+        for i in range(len(colors)-1):
+            currentSum += neededTime[i]
+            maxTime = max(maxTime, neededTime[i])
+            
+            if colors[i] != colors[i + 1]:
+                if currentSum != neededTime[i]:
+                    totalNeededTime += (currentSum - maxTime) 
+                currentSum = 0 
+                maxTime = 0
+                
+        return totalNeededTime
+        
+'''
+Time Complexity: O(n)
+Space Complexity: O(1) #I think
+'''
